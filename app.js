c628b38036ba64c110ca630d65e2010877b779f3
lymine1996 <lymine1996@163.com>
2022-08-08 12:00:51 -0500

    Switch user authentication process to Firebase for better security

--- app.js
+++ app.js
@@ -10,15 +10,18 @@ const db_user = require('./db_user');
 const db_plan = require('./db_plan');
 const SMAA_MCDA = require('./mcda');
 const planRouter = require('./routes/plan');
+const auth = require('./auth');
+const { json } = require('express');
+const email_validation = require("email-validator");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/plan', planRouter);
 
 app.post('/data', async function(req, res, next) {
@@ -124,52 +127,132 @@ app.post('/updateMeasures', async function(req, res, next){
 
 app.post('/register', async function(req, res, next){
 	try{
-		var salt = bcrypt.genSaltSync(12);
-		var hashed_password = bcrypt.hashSync(req.body.password, salt);
-		const result = await db_user.query(
-			`INSERT INTO users(username, password, email, first_name, last_name, is_admin)
-			VALUES ($1, $2, $3, $4, $5, $6)`,
+		const data = await db_user.query(
+			`SELECT EXISTS (SELECT username FROM user_new WHERE LOWER(username)=LOWER($1))::int`,
 			[
 				req.body.username,
-				hashed_password,
-				req.body.email,
-				req.body.first_name,
-				req.body.last_name,
-				req.body.is_admin
 			]
-		);
-		return res.json(result);
+		)
+		if(data.rows[0].exists === 0){
+			const result = await auth.createUser(req.body.username, req.body.email, req.body.password)
+			if(result.status === 'success'){
+				await db_user.query(
+					`INSERT INTO user_new(username, first_name, last_name, is_admin, uid)
+					VALUES ($1, $2, $3, $4, $5)`,
+					[
+						req.body.username,
+						req.body.first_name,
+						req.body.last_name,
+						false,
+						result.info
+					]
+				)
+				const token = await auth.createToken(result.info)
+				if(token.status === 'success'){
+					return res.json(auth.returnError(false, token.info))
+				}
+				else{
+					return res.json(token)
+				}
+			}
+			else{
+				return res.json(result)
+			}
+		}
+		else{
+			return res.json(auth.returnError(true, 'Username already exists.'))
+		}
+		
 	} catch(e) {
 		next(e);
 	}
 })
 
 app.post('/login', async function(req, res, next){
+	// Get email from username
 	try{
-		const result = await db_user.query(
-			`SELECT password
-			FROM users
-			WHERE username = $1`,
-			[req.body.username]
-		);
-		var validLogin = false;
-		if (result.rows.length !== 0) {
-			validLogin = bcrypt.compareSync(req.body.password, result.rows[0].password);
-		};
-		return res.json({
-			credentials: result.rows,
-			validLogin: validLogin
-		});
+		// True means it's an email
+		const email_or_user = email_validation.validate(req.body.username)
+		let email_to_send = req.body.username
+		if(email_or_user === false){
+			const email = await db_user.query(
+				`SELECT email FROM user_new WHERE username = $1`,
+				[
+					req.body.username,
+				]
+			)
+			if(email.rows.length === 0){
+				return res.json(auth.returnError(true, 'Username or password is incorrect'))
+			}
+			email_to_send = email.rows[0].email.replace(/(\r\n|\n|\r)/gm, "")
+			
+		}
+		return res.json(auth.returnError(false, email_to_send))
 	} catch(e) {
 		next(e);
 	}
 })
 
+
+app.post('/getUser', async function(req, res, next){
+	try{
+		const tokenVerification = await auth.verifyToken(req.body.idToken)
+		if(tokenVerification.status === 'success'){
+			let result = await db_user.query(
+				`SELECT username, first_name, last_name
+				FROM user_new
+				WHERE uid = $1`,
+				[tokenVerification.info.uid]
+			);
+			result.rows[0]['email'] = tokenVerification.info.email
+			return res.json(auth.returnError(false, result.rows[0]))
+		}
+		else{
+			return res.json(tokenVerification)
+		}
+	} catch(e) {
+		next(e);
+	}
+})
+
+
+// app.get('/importUsers', async function(req, res, next){
+// 	try{
+// 		const result = await db_user.query(
+// 			`SELECT *
+// 			FROM users WHERE username = 'damiono32'`
+// 		);
+// 		const arr = ['d1e546316ef141b7ae45e3a1b52a6b01']
+// 		var total = 0
+// 		for(let val of result.rows){
+// 			await auth.importUser(val.email, val.password, arr[total])
+// 			await db_user.query(
+// 				`INSERT INTO user_new(username, first_name, last_name, is_admin, uid, email)
+// 				VALUES ($1, $2, $3, $4, $5, $6)`,
+// 				[
+// 					val.username,
+// 					val.first_name,
+// 					val.last_name,
+// 					val.is_admin,
+// 					arr[total],
+// 					val.email
+// 				]
+// 			)
+// 			total += 1;
+// 		}
+		
+// 		res.send(error)
+// 	} catch(e) {
+// 		next(e);
+// 	}
+// })
+
+
 app.post('/user', async function(req, res, next){
 	try{
 		const result = await db_user.query(
 			`SELECT username, email, first_name, last_name, is_admin
-			FROM users
+			FROM users_new
 			WHERE username = $1`,
 			[req.body.username]
 		);
@@ -209,18 +292,42 @@ app.post('/user/report', async function(req, res, next){
 
 app.post('/update/information', async function(req, res, next){
 	try{
-		const result = await db_user.query(
-			`UPDATE users
-			SET email = $2, first_name = $3, last_name = $4
-			WHERE username = $1`,
-			[
-				req.body.username,
-				req.body.email,
-				req.body.first_name,
-				req.body.last_name
-			]
-		);
-		return res.json(result);
+		const token = await auth.verifyToken(req.body.token)
+		if(token.status === 'success'){
+			const data = await db_user.query(
+				`SELECT EXISTS (SELECT email FROM user_new WHERE LOWER(email)=LOWER($1) AND uid != $2)::int`,
+				[
+					req.body.email,
+					token.info.uid
+				]
+			)
+			if(data.rows[0].exists === 0){
+				try {
+					await auth.updateInfo(token.info.uid, req.body.email)
+					const result = await db_user.query(
+						`UPDATE user_new
+						SET email = $2, first_name = $3, last_name = $4
+						WHERE uid = $1`,
+						[
+							token.info.uid,
+							req.body.email,
+							req.body.first_name,
+							req.body.last_name
+						]
+					);
+					return res.json(auth.returnError(false, ''));
+				} catch (error) {
+					return res.json(auth.returnError(true, 'Update failed')) 
+				}
+			}
+			else{
+				return res.json(auth.returnError(true, 'Email already exists'))
+			}
+		}
+		else{
+			
+			return res.json(auth.returnError(true, 'Bad token')) 
+		}	
 	} catch(e) {
 		next(e);
 	}
@@ -228,18 +335,40 @@ app.post('/update/information', async function(req, res, next){
 
 app.post('/update/password', async function(req, res, next){
 	try{
-		var salt = bcrypt.genSaltSync(12);
-		var hashed_password = bcrypt.hashSync(req.body.password, salt);
-		const result = await db_user.query(
-			`UPDATE users
-			SET password = $2
-			WHERE username = $1`,
-			[
-				req.body.username,
-				hashed_password
-			]
-		);
-		return res.json(result);
+		const token = await auth.verifyToken(req.body.token)
+		if(token.status === 'success'){	
+			const pass_up = await auth.updatePassword(token.info.uid, req.body.password)
+			if(pass_up.status === 'success'){
+				return res.json(auth.returnError(false, ''));
+			}
+			else{
+				return res.json(auth.returnError(true, pass_up.info));
+			}
+		}
+		else{
+			return res.json(auth.returnError(true, 'Bad token')) 
+		}
+	} catch(e) {
+		next(e);
+	}
+});
+
+
+app.post('/update/disable', async function(req, res, next){
+	try{
+		const token = await auth.verifyToken(req.body.token)
+		if(token.status === 'success'){	
+			const pass_up = await auth.disableAccount(token.info.uid)
+			if(pass_up.status === 'success'){
+				return res.json(auth.returnError(false, ''));
+			}
+			else{
+				return res.json(auth.returnError(true, pass_up.info));
+			}
+		}
+		else{
+			return res.json(auth.returnError(true, 'Bad token')) 
+		}
 	} catch(e) {
 		next(e);
 	}
