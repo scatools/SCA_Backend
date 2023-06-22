a038d235ae7e115a4c8bb59321869139f050acaf
lymine1996 <lymine1996@163.com>
2023-06-22 15:00:16 -0500

    Switch to new plan database connection

--- db_plan.js
+++ db_plan.js
@@ -17,16 +17,10 @@ const { Client } = require("pg");
 // For production on Heroku
 
 // Need to specify ssl attribute
-var db_plan = new Client({
-  // Need to set these config variables beforehand in Heroku environment settings
-  // For more details, refer to https://devcenter.heroku.com/articles/config-vars
-  user: process.env.PLANDB_USER,
-  password: process.env.PLANDB_PASSWORD,
-  database: "d7r5ouk3pjk073",
-  port: 5432,
-  host: "ec2-3-211-228-251.compute-1.amazonaws.com",
+let db_plan = new Client({
+  connectionString: process.env.HEROKU_POSTGRESQL_CHARCOAL_URL,
   ssl: { rejectUnauthorized: false }
-}); 
+});
 
 db_plan.connect();
 
